ae6da8df604689e92bba107bc01f0afa3bd08f11
zsu <azure.ss@gmail.com>
2017-01-07 14:47:55 -0500

    fix image reference

--- MvcExample/App_Start/BundleConfig.cs
+++ MvcExample/App_Start/BundleConfig.cs
@@ -8,11 +8,11 @@ namespace MvcExample
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+			bundles.Add(new ScriptBundle("~/bundles/jquery","https://code.jquery.com/jquery-1.12.4.js").Include(
 						"~/Scripts/jquery-{version}.js"));
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+			bundles.Add(new ScriptBundle("~/bundles/jqueryui", "https://code.jquery.com/ui/1.12.1/jquery-ui.js").Include(
 						"~/Scripts/jquery-ui-{version}.js"));
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval","https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.js").Include(
 						"~/Scripts/jquery.validate*"));
 			bundles.Add(new ScriptBundle("~/bundles/toastr", "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js").Include(
 						"~/Scripts/toastr.js"));
@@ -28,8 +28,9 @@ namespace MvcExample
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css",
-					  "~/Content/themes/base/jquery-ui.css"));
+					  "~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/themes/base/jquery-ui", "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css")
+				   .Include("~/Content/themes/base/jquery-ui.css"));
 			bundles.Add(new StyleBundle("~/Content/toastr", "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css").Include(
 					  "~/Content/toastr.css"));
 		}
