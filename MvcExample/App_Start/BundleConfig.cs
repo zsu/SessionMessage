d5cd2835830dd7c88c42bde796ba574b66fa74e0
zsu <azure.ss@gmail.com>
2017-01-07 09:56:13 -0500

    add toastr support

--- MvcExample/App_Start/BundleConfig.cs
+++ MvcExample/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace MvcExample
 						"~/Scripts/jquery-ui-{version}.js"));
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
+			bundles.Add(new ScriptBundle("~/bundles/toastr", "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js").Include(
+						"~/Scripts/toastr.js"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -28,6 +30,8 @@ namespace MvcExample
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css",
 					  "~/Content/themes/base/jquery-ui.css"));
+			bundles.Add(new StyleBundle("~/Content/toastr", "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css").Include(
+					  "~/Content/toastr.css"));
 		}
 	}
 }
